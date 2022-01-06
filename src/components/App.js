ffbe3eff16de5dabffa8a65dd50af3241f8d7505
blarth <jminocente@gmail.com>
2022-01-05 23:31:50 -0300

    Tela login requisito concluido, tela cadastro requisito concluido, rotas atualizada

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-import { useState, useContext } from "react";5
+import { useState, useContext } from "react";
 import UserContext from "./contexts/UserContext";
-
+import MainPage from "./MainPage";
+import SignUpPage from "./SignUpPage";
 
 export default function App() {
-  const [token, setToken] = useState("")
+  const [token, setToken] = useState("");
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <UserContext.Provider value={{token, setToken}}>
-            <Route path="/" element={<MainPage />} />
-        </UserContext.Provider>  
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ token, setToken }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
---
+++ src/components/MainPage/index.js
@@ -0,0 +1,143 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import styled from "styled-components";
+import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom"
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+function handleLogin(event) {
+  event.preventDefault();
+}
+
+export default function MainPage() {
+  const [isWaiting , setIsWaiting] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  
+  function handleLogin(event) {
+
+    event.preventDefault();
+    setIsWaiting(true)
+
+    const objectLogin = {
+      email: email,
+      password: password
+    }
+    const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login', 
+  objectLogin
+);
+    promise.then(response => {
+
+        console.log(response)
+        setIsWaiting(false)
+        navigate("/today")
+        
+    }
+    )
+    promise.catch(error => {console.log(error.response)
+        setIsWaiting(false)
+        alert("Login não concluido, tente novamente")
+    
+    });
+}
+  
+
+
+  return (
+    <Container>
+      <Image src={logo} alt="imagem logo"></Image>
+      <Form onSubmit={handleLogin} isWaiting={isWaiting} setIsWaiting={setIsWaiting}>
+        <Input
+        disabled={isWaiting}
+        isWaiting={isWaiting}
+          type="email"
+          required
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+        disabled={isWaiting}
+        isWaiting={isWaiting}
+          type="password"
+          required
+          placeholder="senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isWaiting ? <Button><Loader type="ThreeDots" color="#FFFFFf" height={80} width={80}></Loader></Button> : <Button disabled={isWaiting} type="submit">Entrar</Button> }
+      </Form>
+      <Link to="/signup">
+        <PLink>Não tem uma conta? Cadastre-se!</PLink>
+      </Link>
+    </Container>
+  );
+}
+
+const Container = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  max-width: 50%;
+  max-height: 180px;
+  margin-top: 18%;
+`;
+
+const Input = styled.input`
+  border: 1px solid #d4d4d4;
+  height: 45px;
+  width: 90%;
+  border-radius: 5px;
+
+  ::placeholder {
+    color: #dbdbdb;
+    font-family: Lexend Deca;
+    font-size: 20px;
+    text-align: left;
+  }
+`;
+const Button = styled.button`
+
+height: 45px;
+width: 90%;
+border-radius: 4.6px;
+background: #52B6FF;
+color:#FFFFFF;
+font-size: 21px;
+border: none;
+
+`;
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+width: 100%;
+gap: 6px;
+align-items:center;
+`;
+
+const PLink = styled.p`
+
+height: 17px;
+width: content;
+font-family: Lexend Deca;
+font-size: 14px;
+text-align: center;
+text-decoration-line: underline;
+color: #52B6FF;
+margin-top: 25px;
+
+
+`;
+
+/* 
+const requisicao = axios.post("https://minha-api.com/login", {
+			email: email,
+			senha: senha
+		}); */
